0f9b64fdf773a6c9305a09aa7551e2de9c299473
Alex <Alex@169.254.19.173>
2017-02-24 23:32:38 +0100

    5 version

--- repo/Spring-JDBC/src/com/crunchify/controller/CrunchifyHelloWorld.java
+++ repo/Spring-JDBC/src/com/crunchify/controller/CrunchifyHelloWorld.java
@@ -36,11 +36,7 @@ public class CrunchifyHelloWorld {
 	@RequestMapping(value = { "/", "/welcome" }, method = RequestMethod.GET)
     public String welcomePage(Model model) {
         model.addAttribute("title", "Welcome");
-<<<<<<< HEAD
         model.addAttribute("message", "This is welcome page!!!!!");
-=======
-        model.addAttribute("message", "This is welcome page!!");
->>>>>>> refs/remotes/origin/master
         return "welcomePage";
     }
 	
